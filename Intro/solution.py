4816e23773c81e888a167eeabfb8f0561be02d1f
Dimitris Provatas <james.provatas@gmail.com>
2022-06-25 12:34:58 +0300

    fixed a bug with the puzzle 3 solution not being correct

--- Intro/solution.py
+++ Intro/solution.py
@@ -1,8 +1,18 @@
 #!/Users/sheepstress/miniforge3/bin/python
 
 import csv
+import os
+import time
+from datetime import datetime
 
-file = open("office_database.csv")
+scriptDir = os.path.dirname(__file__)
+filename = "office_database.csv"
+targetFilePath = os.path.join(scriptDir, filename)
+file = open(targetFilePath)
+
+# For Puzzle 3
+targetTime = datetime(1970, 1, 1, 7, 14, 0)
+
 database = csv.DictReader(file, skipinitialspace=True)
 # Row Example: {
 # 'Username': 'abc',
@@ -15,14 +25,16 @@ database = csv.DictReader(file, skipinitialspace=True)
 def main():
     for row in database:
         # Puzzle 3
-        time = row.get("First Login Time")
+        timeString = row.get("First Login Time")
 
-        if time == "99:99":
+        if timeString == "99:99":
             continue
 
-        timeArray = list(map(int, time.split(":")))
+        timeArray = list(map(int, timeString.split(":")))
 
-        if timeArray[0] > 7 or timeArray[1] > 14:
+        timeObj = datetime(1970, 1, 1, timeArray[0], timeArray[1], 0)
+
+        if timeObj.time() > targetTime.time():
             continue
 
         # Puzzle 1
@@ -69,14 +81,16 @@ def puzzle3():
     solution = 0
 
     for row in database:
-        time = row.get("First Login Time")
+        timeString = row.get("First Login Time")
 
-        if time == "99:99":
+        if timeString == "99:99":
             continue
 
-        timeArray = list(map(int, time.split(":")))
+        timeArray = list(map(int, timeString.split(":")))
 
-        if timeArray[0] > 7 or timeArray[1] > 14:
+        timeObj = datetime(1970, 1, 1, timeArray[0], timeArray[1], 0)
+
+        if timeObj.time() > targetTime.time():
             continue
 
         solution += int(row.get("ID"))
@@ -85,7 +99,7 @@ def puzzle3():
 
 
 if __name__ == "__main__":
-    # main()
+    main()
     # puzzle1()
     # puzzle2()
-    puzzle3()
+    # puzzle3()
